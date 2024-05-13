7956514ec4137f9ba9843d622c0200d461f709cf
Shubham <shubham.shubham.singh13@gmail.com>
2024-05-13 06:19:16 +0530

    data modeling completed

---
+++ node_cpc/models/hostpital_managment_system/hospital.models.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    pinCode: { type: String, required: true },
+  },
+  { type: true }
+);
+
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
