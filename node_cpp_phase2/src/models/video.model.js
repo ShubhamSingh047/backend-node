75b0955e84414b3416aaf9a7bc882e90d60c4807
Shubham <shubham.shubham.singh13@gmail.com>
2024-05-16 23:49:59 +0530

    some changes

--- node_cpp_phase2/src/models/video.model.js
+++ node_cpp_phase2/src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema, model } from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -41,5 +41,6 @@ const videoSchema = new Schema(
   { timestamps: true }
 );
 
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = model("Video", videoSchema);
